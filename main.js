960b26a7cc0116ff6fd34be6031680206ade2f02
FalseFaz <francescoarzilli2005@gmail.com>
2023-10-22 16:41:46 +0200

    V1

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const QRCode = require("qrcode");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: "icon.ico",
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    mainWindow.removeMenu(true);
+    mainWindow.loadFile("index.html");
+
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+}
+
+app.on("ready", createWindow);
+
+app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.on("generateQRCode", async (event, url) => {
+    try {
+        const imageData = await QRCode.toDataURL(url);
+        event.reply("showQRCode", imageData);
+    } catch (error) {
+        console.error(error);
+    }
+});
---
+++ script.js
@@ -0,0 +1,67 @@
+const { ipcRenderer } = require("electron");
+
+const url = document.querySelector("#url");
+const generateBtn = document.querySelector("#generateBtn");
+const downloadBtn = document.querySelector("#downloadBtn");
+const qrcode = document.querySelector("#qrcode");
+const notificationBanner = document.querySelector("#notificationBanner");
+
+const notificationBanner_text = "Image copied to clipboard!";
+const notificationBanner_time = 2000;
+
+let imageData = null;
+
+function showCopiedBanner(text, time) {
+    notificationBanner.textContent = text;
+    notificationBanner.style.display = "block";
+    setTimeout(() => (notificationBanner.style.display = "none"), time);
+}
+
+generateBtn.addEventListener("click", () => {
+    if (url.value !== "") ipcRenderer.send("generateQRCode", url.value);
+});
+
+downloadBtn.addEventListener("click", () => {
+    if (imageData) {
+        const link = document.createElement("a");
+        link.download = "qrcode.png";
+        link.href = imageData;
+        link.click();
+    } else console.error("No QR code available to download");
+});
+
+ipcRenderer.on("showQRCode", (event, data) => {
+    imageData = data;
+    const img = document.createElement("img");
+    img.id = "qrimg";
+    img.src = imageData;
+    img.alt = "QR Code";
+    qrcode.innerHTML = "";
+    qrcode.appendChild(img);
+    downloadBtn.style.display = "flex";
+});
+
+qrcode.addEventListener("click", () => {
+    if (imageData) {
+        const img = new Image();
+        img.src = imageData;
+
+        img.onload = () => {
+            const canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+
+            canvas.toBlob((blob) => {
+                const item = new ClipboardItem({ "image/png": blob });
+                navigator.clipboard
+                    .write([item])
+                    .then(() => showCopiedBanner(notificationBanner_text, notificationBanner_time))
+                    .catch((error) => {
+                        console.error("Unable to copy image to clipboard", error);
+                    });
+            });
+        };
+    }
+});
